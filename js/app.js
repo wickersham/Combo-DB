fadf6f44880bd8013f8940d8b5366c00dc5c2f92
wickersham <wickersham90@gmail.com>
2015-09-24 19:03:01 -0400

    prototype pages with minimal content

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var combotemplate = {
+    character: "ryu",
+    game: "sf5",
+    notation: "cMK fireball",
+};
+
+
+var quotebank = ["you can never lose with a good enough excuse", "that's why i dont go outside", "florida is not part of the east coast", "having trouble with an opponent who blocks too much? try using a throw!", "the answer lies in the heart of battle"];
